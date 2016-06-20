c9109f3e206e5ec9aef193594ff2829580e6e8eb
Loys <toto@asus.netgear.com>
2016-06-20 23:00:49 +0200

    réalisation test

---
+++ javaproject/main/test/view/ViewFrameTest.java
@@ -0,0 +1,99 @@
+package view;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ViewFrameTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testViewFrameIModel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testViewFrameIModelGraphicsConfiguration() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testViewFrameIModelString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testViewFrameIModelStringGraphicsConfiguration() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetController() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetController() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetModel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testPrintMessage() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testKeyTyped() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testKeyPressed() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testKeyReleased() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testAfficherScore() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetScore() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetScore() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetVie() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetVie() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
